5bcbd7b392a90cc3930cc7fedf07e49bff13d29b
codex123 <you@example.com>
2020-09-10 13:05:25 +0200

    new changes

---
+++ greet.js
@@ -0,0 +1,40 @@
+module.exports = function greet() {
+
+    var names = {}
+
+    function greeter(name, language) {
+        if (!name) {
+            return "no name entered";
+        }
+        addNames(name)
+
+        if (language === "isiXhosa") {
+            return "Molo"+", " + name
+        }
+        if (language === "English") {
+            return "Hello"+", " + name
+        }
+
+        if (language === "isiZulu") {
+            return "Sawubona"+", " + name
+        }
+    }
+    function addNames(name) {
+
+        if (names[name] === undefined) {
+            names[name] = 0;
+        }
+        names[name]++
+        console.log(names)
+    }
+
+    function countAll() {
+        return Object.keys(names).length
+    }
+
+    return {
+        greeter,
+        addNames,
+        countAll
+    }
+}
--- index.js
+++ index.js
@@ -1,4 +1,51 @@
-app.get('/the-route', function (req, res) {
-    req.flash('info', 'Flash Message Added');
-    res.redirect('/');
+let express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const greetings = require('./greet')
+
+const greet = greetings()
+let app = express();
+
+app.engine('handlebars', exphbs({
+    layoutsDir: './views/layouts'
+}));
+
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+    res.render('index', {
+
+    });
+});
+
+app.post('/greeted', function (req, res) {
+
+    let firstName = req.body.greeted;
+    let languages = req.body.language;
+
+    res.render("index", { greet: greet.greeter(firstName, languages),  count: greet.countAll()})
+})
+
+app.post('/counter/Lulama', function (req, res) {
+
+
+})
+
+app.post('/counter', function (req, res) {
+
+    let count = req.body.counted;
+    res.render("index", { greet: greet.countAll(count) })
+})
+
+
+
+let PORT = process.env.PORT || 3007;
+
+app.listen(PORT, function () {
+    console.log('App starting on port', PORT);
 });
